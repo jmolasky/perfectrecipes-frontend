d82c0f4f649e05f2385d0c3e48bfe313acf49fe4
jmolasky <jeromemolasky@gmail.com>
2022-01-13 15:46:26 -0600

    add index page logic

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,3 +1,31 @@
+import { useEffect, useState } from "react";
+import { Route, Switch } from "react-router-dom";
+import Index from "../pages/Index";
+import Show from "../pages/Show";
+
 export default function Main(props) {
-  return <h1>Main</h1>;
+  const [recipes, setRecipes] = useState([]);
+  const URL = "https://perfectrecipes-backend.herokuapp.com/";
+
+  const getRecipes = async () => {
+    const response = await fetch(URL);
+    const data = await response.json();
+    setRecipes(data);
+  };
+
+  useEffect(() => getRecipes(), []);
+
+  return (
+    <main>
+      <Switch>
+        <Route exact path="/">
+          <Index recipes={recipes} />
+        </Route>
+        <Route
+          path="/:id"
+          render={(rp) => <Show {...rp} recipes={recipes} />}
+        />
+      </Switch>
+    </main>
+  );
 }
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,3 +1,19 @@
+import { Link } from "react-router-dom";
+
 export default function Index(props) {
-  return <h1>Index Page</h1>;
+  const loaded = () => {
+    return props.recipes.map((recipe) => (
+      <div key={recipe._id} className="recipe">
+        <Link to={`/${recipe._id}`}>
+          <h1>{recipe.name}</h1>
+        </Link>
+        <img src={recipe.image} alt={recipe.name} />
+      </div>
+    ));
+  };
+
+  const loading = () => {
+    return <h1>Loading...</h1>;
+  };
+  return props.recipes ? loaded() : loading();
 }
