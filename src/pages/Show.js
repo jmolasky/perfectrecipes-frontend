af6b8689886fc2da81517dbf87a90c9af10c2c26
jmolasky <jeromemolasky@gmail.com>
2022-01-14 15:54:18 -0600

    add ingredient and instruction capitalization

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,23 +1,26 @@
 export default function Show(props) {
   const id = props.match.params.id;
   const recipe = props.recipes.find((recipe) => recipe._id === id);
+  function capitalizeFirstLtr(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
 
   const ingredientList = recipe.ingredients.map((ingredient) => {
+    let fullIngredient;
     let prefix;
     if (ingredient.measurement || ingredient.amount) {
       prefix = ingredient.measurement
         ? ingredient.measurement
         : ingredient.amount;
+      fullIngredient = capitalizeFirstLtr(`${prefix} ${ingredient.name}`);
+    } else {
+      fullIngredient = capitalizeFirstLtr(ingredient.name);
     }
-    return (
-      <li>
-        {prefix} {ingredient.name}
-      </li>
-    );
+    return <li>{fullIngredient}</li>;
   });
 
   const instructions = recipe.instructions.map((instruction) => (
-    <li>{instruction}</li>
+    <li>{capitalizeFirstLtr(instruction)}</li>
   ));
 
   return (
