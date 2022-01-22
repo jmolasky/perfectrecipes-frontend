0f953fd19a04ddb7584a365af141829d7b9ca811
jmolasky <jeromemolasky@gmail.com>
2022-01-22 09:48:18 -0600

    improve sign in page functionality

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,24 +1,37 @@
 import { loginWithGoogle, auth } from "../services/firebase";
-import { useRef } from "react";
+import { useState } from "react";
 
 export default function Login(props) {
-  const emailRef = useRef(null);
-  const passwordRef = useRef(null);
+  const [loginCredentials, setLoginCredentials] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (evt) => {
+    setLoginCredentials({
+      ...loginCredentials,
+      [evt.target.name]: evt.target.value,
+    });
+  };
 
   const login = (evt) => {
     evt.preventDefault();
-    auth.signInWithEmailAndPassword(
-      emailRef.current.value,
-      passwordRef.current.value
-    );
+    try {
+      auth.signInWithEmailAndPassword(
+        loginCredentials.email,
+        loginCredentials.password
+      );
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   const signup = (evt) => {
     evt.preventDefault();
     try {
       auth.createUserWithEmailAndPassword(
-        emailRef.current.value,
-        passwordRef.current.value
+        loginCredentials.email,
+        loginCredentials.password
       );
     } catch (err) {
       console.log(err);
@@ -26,20 +39,48 @@ export default function Login(props) {
   };
 
   return (
-    <div className="login">
-      <form>
-        <h1>Sign In</h1>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+      className="login"
+    >
+      <form style={{ display: "flex", flexDirection: "column", width: "50%" }}>
+        <h1 style={{ textAlign: "center" }}>Sign In</h1>
         <label htmlFor="email">Email</label>
-        <br />
-        <input ref={emailRef} type="email" placeholder="email" />
-        <br />
+        <input
+          name="email"
+          value={loginCredentials.email}
+          type="email"
+          onChange={handleChange}
+        />
         <label htmlFor="password">Password</label>
-        <br />
-        <input ref={passwordRef} type="password" placeholder="password" />
-        <br />
-        <button onClick={login}>Sign In</button>
-        <button onClick={signup}>Sign Up</button>
+        <input
+          name="password"
+          value={loginCredentials.password}
+          type="password"
+          onChange={handleChange}
+        />
+        <button
+          disabled={
+            loginCredentials.email && loginCredentials.password ? false : true
+          }
+          onClick={login}
+        >
+          Sign In
+        </button>
+        <button
+          disabled={
+            loginCredentials.email && loginCredentials.password ? false : true
+          }
+          onClick={signup}
+        >
+          Sign Up
+        </button>
       </form>
+      <br />
       <button onClick={loginWithGoogle}>Log in with Google</button>
     </div>
   );
