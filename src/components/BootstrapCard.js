697af101284b7ccf35017afb92f146d98690aad7
jmolasky <jeromemolasky@gmail.com>
2022-01-28 14:55:23 -0600

    implement react-bootstrap cards

--- src/components/BootstrapCard.js
+++ src/components/BootstrapCard.js
@@ -1,49 +1,64 @@
 import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
 
 export default function BootstrapCard(props) {
   return (
-    <Card
-      style={{
-        width: "100%",
-        height: "15rem",
-        // paddingTop: "66.66%",
-        border: "2px solid grey",
-        margin: ".75rem",
-      }}
-      onClick={(e) => {
-        props.handleClick(props.recipe);
-      }}
-    >
-      <Card.Img
-        style={{ height: "100%", objectFit: "cover" }}
-        src={props.recipe.image}
-        alt={props.recipeName}
-      />
-      <Card.ImgOverlay
+    <Col className="mb-4 recipe-card" sm={4} md={3} lg={2}>
+      <Card
+        className="h-100"
         style={{
-          padding: "0",
-          display: "flex",
+          // width: "100%",
+          // height: "100%",
+          // paddingTop: "66.66%",
+          border: "2px solid grey",
+          // margin: ".75rem",
+        }}
+        onClick={(e) => {
+          props.handleClick(props.recipe);
         }}
       >
-        <Card.Title
+        <Card.Img
+          style={{ height: "100%", objectFit: "cover" }}
+          src={props.recipe.image}
+          alt={props.recipeName}
+        />
+        <Card.ImgOverlay
           style={{
-            width: "100%",
-            backgroundColor: "#575757c9",
-            color: "#d8d8d8",
-            paddingTop: "1rem",
-            paddingBottom: "1rem",
-            paddingLeft: "1rem",
-            paddingRight: "1rem",
-            margin: "0",
-            overflow: "hidden",
-            whiteSpace: "nowrap",
-            textOverflow: "ellipsis",
+            padding: "0",
+            display: "flex",
           }}
-          className="text-center align-self-end"
         >
-          {props.recipeName}
-        </Card.Title>
-      </Card.ImgOverlay>
-    </Card>
+          <Card.Title
+            // "as" gives sass stylesheet access
+            as="div"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              width: "100%",
+              height: "25%",
+              backgroundColor: "#575757c9",
+              color: "#d8d8d8",
+              // paddingTop: "1rem",
+              // paddingBottom: "1rem",
+              paddingLeft: ".5rem",
+              paddingRight: ".5rem",
+              margin: "0",
+            }}
+            className="text-center align-self-end card-title"
+          >
+            <div
+              style={{
+                overflow: "hidden",
+                whiteSpace: "nowrap",
+                textOverflow: "ellipsis",
+              }}
+            >
+              {props.recipeName}
+            </div>
+          </Card.Title>
+        </Card.ImgOverlay>
+      </Card>
+    </Col>
   );
 }
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,6 +1,8 @@
 import RecipeCard from "../components/RecipeCard";
 import Container from "react-bootstrap/Container";
 import BootstrapCard from "../components/BootstrapCard";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default function Index(props) {
   const handleClick = (recipe) => {
@@ -14,33 +16,46 @@ export default function Index(props) {
     //   handleClick={handleClick}
     //   recipeName={recipe.name}
     // />
+    // <Col key={recipe._id} className="mb-4 recipe-card" sm={4} md={3} lg={2}>
     <BootstrapCard
       key={recipe._id}
       recipe={recipe}
       handleClick={handleClick}
       recipeName={recipe.name}
     />
+    // </Col>
   ));
 
   const loaded = () => {
     return (
+      // <Container>
+      //   {props.user.displayName && (
+      //     <h2 style={{ textAlign: "center" }}>
+      //       Welcome, {props.user.displayName.split(" ")[0]}!
+      //     </h2>
+      //   )}
+      //   <div
+      //     id="recipes-container"
+      //     style={{
+      //       display: "flex",
+      //       flexWrap: "wrap",
+      //       alignItems: "center",
+      //       justifyContent: "center",
+      //     }}
+      //   >
+      //     {recipes}
+      //   </div>
+      // </Container>
       <Container>
         {props.user.displayName && (
           <h2 style={{ textAlign: "center" }}>
             Welcome, {props.user.displayName.split(" ")[0]}!
           </h2>
         )}
-        <div
-          id="recipes-container"
-          style={{
-            display: "flex",
-            flexWrap: "wrap",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
+        {/* <Container className="row row-cols-1 row-cols-md-2 row-cols-lg-3">
           {recipes}
-        </div>
+        </Container> */}
+        <Row>{recipes}</Row>
       </Container>
     );
   };
